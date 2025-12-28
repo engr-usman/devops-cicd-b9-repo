a4aef162268196e892927463dff973b17b11acef
Usman Ahmad <engr.syedusmanahmad@gmail.com>
2025-12-28 16:04:34 +0500

    complete cicd using ecr and ec2

--- docker-python-app-code/app.py
+++ docker-python-app-code/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker + GitHub Actions!"
+    return "Hello from LbisTech - This is CICD Training using Github Actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
